30b6071139dc11d60fefa588e1bbb579d5488248
Jean Dresbach <jean.rafael.dresbach@gmail.com>
2025-02-07 20:30:20 -0300

    call to api configured

--- src/services/api.js
+++ src/services/api.js
@@ -5,6 +5,18 @@ const api = axios.create({
     timeout: 5000,
 })
 
+// Adiciona o token de autorização ao cabeçalho automaticamente
+api.interceptors.request.use(config => {
+    const token = localStorage.getItem("token")
+
+    // Ignora as rotas de login e signup
+    if (!["/login", "/signup"].includes(config.url) && token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
+
+    return config
+})
+
 export const login = async (email, password) => {
     const response = await api.post("/login", { email, password })
     return response.data
@@ -22,5 +34,37 @@ export const fetchTransactions = async () => {
     } catch (error) {
         console.error("Erro ao buscar as transações:", error)
         return []
+    }
+}
+
+export const createTransaction = async transaction => {
+    try {
+        const response = await api.post("/transactions", transaction)
+        return response.data
+    } catch (error) {
+        console.error("Erro ao criar transação:", error)
+        throw error
+    }
+}
+
+export const updateTransaction = async (transactionId, updatedTransaction) => {
+    try {
+        const response = await api.put(
+            `/transactions/${transactionId}`,
+            updatedTransaction
+        )
+        return response.data
+    } catch (error) {
+        console.error("Erro ao atualizar transação:", error)
+        throw error
+    }
+}
+
+export const deleteTransaction = async transactionId => {
+    try {
+        await api.delete(`/transactions/${transactionId}`)
+    } catch (error) {
+        console.error("Erro ao excluir transação:", error)
+        throw error
     }
 }
